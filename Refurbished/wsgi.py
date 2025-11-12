f01b31a7dbafd4f451ada905f42d6efbcb3ae849
francesco.lavatelli <francesco.lavatelli@empsol.it>
2025-11-12 09:43:09 +0100

    first commit

--- Refurbished/wsgi.py
+++ Refurbished/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import dotenv
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # project root
+dotenv.load_dotenv(BASE_DIR / '.env')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Refurbished.settings')
 
